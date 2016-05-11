6df59765234f6fd2a7761bb6dc6c64752d9bd50d
patterncat <patterncat@gmail.com>
2016-05-11 17:36:59 +0800

    add log support

--- src/main/java/com/patterncat/SpringbootDockerDemoApplication.java
+++ src/main/java/com/patterncat/SpringbootDockerDemoApplication.java
@@ -1,5 +1,7 @@
 package com.patterncat;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,12 +11,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class SpringbootDockerDemoApplication {
 
+	private static final Logger logger = LoggerFactory.getLogger(SpringbootDockerDemoApplication.class);
+
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootDockerDemoApplication.class, args);
 	}
 
 	@RequestMapping("/")
 	public String index(){
+		logger.info("request /");
 		return "this is index";
 	}
 }
